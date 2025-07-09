90d32afabf9dc65366f8f1962d4f6f70f678dbfb
tynnvemp <hnam23102004@gmail.com>
2025-07-09 18:04:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -52,6 +52,9 @@ async def on_ready():
     await bot.tree.sync()
     print(f"✅ Bot đã sẵn sàng: {bot.user}")
 
+from chess_bot_game import setup_chess_ai_commands
+setup_chess_ai_commands(bot)
+
 from chess_game import setup_chess_commands
 setup_chess_commands(bot)
 
